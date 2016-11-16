dc53ad9a259361d09089ff89d6d65820cd53a759
Patryk Ostrowski <ostrowski.patryk@gmail.com>
2016-11-16 10:38:07 +0100

    Uzupełnienie wzoru o licznie miejsc zerowych i współrzędnych wierzchołka paraboli.

--- TrojmianKwadratowy.cpp
+++ TrojmianKwadratowy.cpp
@@ -1,19 +1,34 @@
 #include <iostream>
 #include <conio.h>
+#include <math.h>
 
-int delta(int a, int b, int c);
+int wyliczDelte(int a, int b, int c);
+int wyliczPierwszeMiejsceZerowe(int wynikDelty, int a, int b);
+int wyliczDrugieMiejsceZerowe(int wynikDelty, int a, int b);
+void wyliczWspolrzedneWierzcholka(int wynikDelty, int a, int b);
+int a, b, c;
 
 int main()
 {
-	int wynikDelty = delta(3, 4, 5);
+	a = -3, b = 4, c = 5;
+
+	int wynikDelty = wyliczDelte(a, b, c);
+
 	std::cout << "Delta wynosi " << wynikDelty << std::endl;
 	if (wynikDelty > 0)
 	{
 		std::cout << "Mamy dwa miejsca zerowe" << std::endl;
+		int pierwszeMiejsce = wyliczPierwszeMiejsceZerowe(wynikDelty, a, b);
+		int drugieMiejsce = wyliczDrugieMiejsceZerowe(wynikDelty, a, b);
+		std::cout << "Miejsca zerowe to " << pierwszeMiejsce << " i " << drugieMiejsce << std::endl;
+		wyliczWspolrzedneWierzcholka(wynikDelty, a, b);
 	}
 	else if (wynikDelty == 0)
 	{
 		std::cout << "Mamy jedno miejsce zerowe" << std::endl;
+		int pierwszeMiejsce = wyliczPierwszeMiejsceZerowe(wynikDelty, a, b);
+		std::cout << "Miejsce zerowe to " << pierwszeMiejsce << std::endl;
+		wyliczWspolrzedneWierzcholka(wynikDelty, a, b);
 	}
 	else
 	{
@@ -24,8 +39,29 @@ int main()
     return 0;
 }
 
-int delta(int a, int b, int c)
+int wyliczDelte(int a, int b, int c)
 {
 	int delta = b * b - 4 * a * c;
 	return delta;
 }
+
+int wyliczPierwszeMiejsceZerowe(int wynikDelty, int a, int b)
+{
+    int pierwszeMiejsceZerowe = ((-b - sqrt(wynikDelty)) / 2 * a);
+    return pierwszeMiejsceZerowe;
+}
+
+
+int wyliczDrugieMiejsceZerowe(int wynikDelty, int a, int b)
+{
+	int drugieMiejsceZerowe = ((-b + sqrt(wynikDelty)) / 2 * a);
+	return drugieMiejsceZerowe;
+};
+
+void wyliczWspolrzedneWierzcholka(int wynikDelty, int a, int b)
+{
+	int wspolrzednaX, wspolrzednaY;
+	wspolrzednaX = (- b) / (2 * a);
+	wspolrzednaY = (- wynikDelty) / (4 * a);
+	std::cout << "Wspolrzedne wierzcholka paraboli: X = " << wspolrzednaX << ", Y = " << wspolrzednaY << std::endl;
+};
