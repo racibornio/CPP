4512d1e33295a93d6f7ff60e30af57d63db3c663
Patryk Ostrowski (External) <paostrowski@blackberry.com>
2016-11-27 15:33:34 +0100

    Nowy plik - drugi z laborki.

---
+++ zad_2.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <cmath>
+
+double a, b, c;
+void pobierzDlugosciBokow();
+bool sprawdzCzyProstokatny();
+
+int main()
+{
+    pobierzDlugosciBokow();
+    std::cout << "a=" << a << ", b=" << b << ", c=" << c << std::endl;
+    sprawdzCzyProstokatny();
+    return 0;
+}
+
+void pobierzDlugosciBokow()
+{
+    using namespace std;
+
+    while(a==0)
+    {
+        cout << "Podaj dlugosc boku A: ";
+        cin >> a;
+    }
+
+    while(b==0)
+    {
+        cout << "Podaj dlugosc boku B: ";
+        cin >> b;
+    }
+
+    while(c==0)
+    {
+        cout << "Podaj dlugosc boku C: ";
+        cin >> c;
+    }
+}
+
+bool sprawdzCzyProstokatny()
+{
+    using namespace std;
+
+    if (a*a == (b*b) + (c*c))
+    {
+        cout << "Trojkat jest prostokatny dla a=" << a << " jako przeciwprotokatnej." << endl;
+        return true;
+    }
+    else if (b*b == (a*a) + (c*c))
+    {
+        cout << "Trojkat jest prostokatny dla b=" << b << " jako przeciwprostokatnej." << endl;
+        return true;
+    }
+    else if (c*c == (a*a) + (b*b))
+    {
+        cout << "Trojkat jest prostokatny dla c=" << c << " jako przeciwprostokatnej." << endl;
+        return true;
+    }
+    else
+    {
+        cout << "To nie jest trojkat prostokatny." << endl;
+        return false;
+    }
+
+}
